9b0c1b33e27f8e8f3bc4cf849c44b107f3c6f90c
adidas <314475440@qq.com>
2022-05-07 22:51:41 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod png;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    todo!()
 }
