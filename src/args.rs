7eba9196f981f61b0d3cfd5f641b1347040eae67
adidas <314475440@qq.com>
2022-05-11 12:47:23 +0800

    cli args

--- src/args.rs
+++ src/args.rs
@@ -1,41 +1,45 @@
 use clap::Parser;
+use clap::Subcommand;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[clap(name = "pngme")]
-#[clap(bin_name = "pngme")]
-pub enum PngMeArgs {
+#[clap(author, version, about, long_about = None)]
+pub struct PngMeArgs {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
     Encode(EncodeArgs),
     Decode(DecodeArgs),
     Remove(RemoveArgs),
     Print(PrintArgs),
 }
 
-#[derive(clap::Args)]
-#[clap(author, version, about, long_about = None)]
+#[derive(clap::Args, Debug)]
 pub struct EncodeArgs {
     file_path: PathBuf,
     chunk_type: String,
     message: String,
+    #[clap(short, long, parse(from_os_str), value_name = "OUTPUT")]
     output_file: Option<PathBuf>,
 }
 
-#[derive(clap::Args)]
-#[clap(author, version, about, long_about = None)]
+#[derive(clap::Args, Debug)]
 pub struct DecodeArgs {
     file_path: PathBuf,
     chunk_type: String,
 }
 
-#[derive(clap::Args)]
-#[clap(author, version, about, long_about = None)]
+#[derive(clap::Args, Debug)]
 pub struct RemoveArgs {
     file_path: PathBuf,
     chunk_type: String,
 }
 
-#[derive(clap::Args)]
-#[clap(author, version, about, long_about = None)]
+#[derive(clap::Args, Debug)]
 pub struct PrintArgs {
     file_path: PathBuf,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,26 @@ mod commands;
 mod png;
 
 use anyhow::Result;
+use args::{Commands, PngMeArgs};
+use clap::StructOpt;
 fn main() -> Result<()> {
-    todo!()
+    let args = PngMeArgs::parse();
+    match &args.command {
+        Commands::Encode(arg) => {
+            println!("{:?}", &arg);
+            Ok(())
+        }
+        Commands::Decode(arg) => {
+            println!("{:?}", &arg);
+            Ok(())
+        }
+        Commands::Remove(arg) => {
+            println!("{:?}", &arg);
+            Ok(())
+        }
+        Commands::Print(arg) => {
+            println!("{:?}", &arg);
+            Ok(())
+        }
+    }
 }
